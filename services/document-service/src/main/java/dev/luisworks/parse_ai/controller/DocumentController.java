9f57c36daba1c60782cda311ea69b4b962f7614b
LuiisCarlos <luisccg2003@gmail.com>
2025-07-20 00:40:34 +0200

    fix config and duplicated method bug + adding profiles to document-service pom.xml for common lib

--- services/document-service/src/main/java/dev/luisworks/parse_ai/controller/DocumentController.java
+++ services/document-service/src/main/java/dev/luisworks/parse_ai/controller/DocumentController.java
@@ -1,7 +1,5 @@
 package dev.luisworks.parse_ai.controller;
 
-import java.util.Set;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import dev.luisworks.parse_ai.dto.DocumentResponse;
 import dev.luisworks.parse_ai.service.DocumentService;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,12 +30,12 @@ public class DocumentController {
     public ResponseEntity<String> get() {
         return ResponseEntity.ok("Document-service response");
     }
-
+/*
     @PostMapping(value = "/upload")
     public ResponseEntity<String> uploadAll(Set<MultipartFile> multiparFile) {
 
         return null;
-    }
+    } */
 
     @PostMapping(value = "/upload")
     public ResponseEntity<DocumentResponse> upload(MultipartFile multiparFile) {
--- services/document-service/src/main/java/dev/luisworks/parse_ai/shared/util/JsonValuedEnum.java
+++
@@ -1,5 +0,0 @@
-package dev.luisworks.parse_ai.shared.util;
-
-public interface JsonValuedEnum {
-    String value();
-}
